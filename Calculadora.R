b062f91428b2eaf760114f25e45266828eb9b666
Cristian Rubio Fuenzalida <crubio@servexternos.santandertecnologia.com.ar>
2021-11-19 14:11:12 -0300

    Ejemplos de uso de RStudio como calculadora

---
+++ Calculadora.R
@@ -0,0 +1,70 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+2^3*5
+
+2^-4
+
+2^(-4)
+
+2*pi
+
+#Infinito
+Inf
+
+NA
+
+NaN
+
+5/0
+
+0/0
+
+2^50
+
+2^(-15)
+
+#Vectores
+c(2^30, 2^(-15), 1, 2, 3/2)
+
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+factorial(5)
+
+#Coeficiente binomial
+
+choose(5,3)
+
+choose(3,5)
+
+#Funciones trigonométricas (usan argumentos en radianes)
+
+
+sin(pi/2)
+
+tan(0)
+
+cos(60*pi/180)
+
+sin(60*pi/180)
+
+sinpi(1/2) # es igual a sin(pi/2)
+
+tan(pi/2) # == 1.633124e+16 se interpreta como infinito (Inf)
+
+asin(0.8660254) # arcoseno en radianes
+
+asin(0.8660254) * 180/pi # arcoseno en grados
+
+asin(5) #Error, ya que arcoseno(x) x está entre -1 y 1
